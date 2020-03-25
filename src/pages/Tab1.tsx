632fc2c816e67c4f3ad6d2ddf0c3ceec3918613e
webdevabordo <jalmeida@allabordo.com>
2020-03-24 17:21:46 -0700

    library to let prometheus pull data

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar , IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Histogram } from 'prom-client'
 import './Tab1.css';
 import { tsConstructorType } from '@babel/types';
 
@@ -9,10 +10,18 @@ const Tab1: React.FC = () => {
 
   const [ barcode , setBarcode ] = useState('');
   
-  const openScanner = async () => {
-    const data = await BarcodeScanner.scan();
-    setBarcode(data.text);
-  }
+  const httpRequestDurationMicroseconds = new Histogram({
+     name: 'http_request_duration_ms',
+     help: 'Duration of HTTP requests in ms',
+     labelNames:['route'],
+     //buckets for reponse time
+     buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]
+  });
+
+  httpRequestDurationMicroseconds.observe(10);
+
+  console.log(httpRequestDurationMicroseconds)
+  
 
   return (
     <IonPage>
@@ -25,20 +34,9 @@ const Tab1: React.FC = () => {
         <div>
           <h1>Data:{barcode}</h1>
         </div>
-        {/* <div>
-          <h1>Y</h1>
-        </div>
-        <div>
-          <h1>Z</h1>
-        </div>
-        <div>
-          <h1>TimeStamp</h1>
-        </div> */}
-
         
-        
-        <IonButton onClick={openScanner}>Read barcode</IonButton>
-        {/* <IonButton onClick={stopMonitoring}>Stop monitoring</IonButton> */}
+        <IonButton>Read barcode</IonButton>
+       
       </IonContent>
     </IonPage>
   );
